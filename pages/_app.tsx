005450acb21b728a86eafae32b5e2cee19a24fb5
lavercourage <smqkemfht@gmail.com>
2022-03-26 01:12:14 +0900

    220326 : [modify] 파이어베이스 앱에 프롭스로 넘겨주기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import "../styles/globals.css";
 import Layout from "../src/components/commons/layout"; // 레이아웃 디자인(헤더)
 import { Global } from "@emotion/react"; // emotion Grobal 설정
 import { globalStyles } from "../src/commons/styles/globalStyles"; // 글로벌 스타일
-// import
+import { app, db } from "../src/commons/firebase/firebaseConfig";
 
 function MyApp({ Component, pageProps }: any) {
-  console.log();
+  console.log("app", app, "db", db);
   return (
     <>
       <Global styles={globalStyles} />
