7563011f521993f0338b8a279b2792591d5c9c74
lavercourage <smqkemfht@gmail.com>
2022-03-25 23:59:48 +0900

    220325 : [update] firebase 기초 설정 진행 중

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
-import { Global } from "@emotion/react";
-import Layout from "../src/components/commons/layout";
-import { globalStyles } from "../src/commons/styles/globalStyles";
+import Layout from "../src/components/commons/layout"; // 레이아웃 디자인(헤더)
+import { Global } from "@emotion/react"; // emotion Grobal 설정
+import { globalStyles } from "../src/commons/styles/globalStyles"; // 글로벌 스타일
+// import
 
 function MyApp({ Component, pageProps }: any) {
+  console.log();
   return (
     <>
       <Global styles={globalStyles} />
